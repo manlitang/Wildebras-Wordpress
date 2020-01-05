e0595d1e66bb6f47f3b13518ddb79fe81e34ea34
manlitang <44969715+manlitang@users.noreply.github.com>
2020-01-05 11:11:35 +0100

    burger menu border radius

--- header.php
+++ header.php
@@ -22,139 +22,138 @@
         </a>
 
         <div class="nav-links nav-links--mobile">
-    <ul>
-        <div class="search-bar search-bar--mobile">
-            <input type="text" placeholder="Search product...">
-            <button type="submit"><img src="<?php bloginfo('template_directory');?>/images/search.svg" alt="Search product"></button>
-        </div>
-
-        <?php $locations = get_nav_menu_locations();
-        // if there's a location for the primary menu
-        if (isset( $locations['top-menu'])) {
-            $menu = get_term( $locations['top-menu'], 'nav_menu');
-            // if there are items in the primary menu
-            if ($items = wp_get_nav_menu_items($menu->name)) {
-                // loop through all menu items to display their content
-                foreach ($items as $item) {
-                    // if the current item is not a top level item, skip it
-                    if ($item->menu_item_parent != 0) {
-                        continue;
-                    }
-                    // get the ID of the current nav item
-                    $curNavItemID = $item->ID;
-                    // get the custom classes for the item
-                    $classList = implode(" ", $item->classes);
-                    echo "<li class=\"{$classList}\">";
-                    echo "<a href=\"{$item->url}\">{$item->title}</a>";
-                    // build the mega-menu
-                    // if 'mega-menu'  exists within the class
-                    if (in_array('is-mega-menu', $item->classes)) { ?>
-                        <div class="mega-menu">
-                            <div class="<?php 
-                                if ($item->title == "Shop vintage") {
-                                    echo "mega-menu-column--small";
-                                }
-                                if ($item->title == "Shop collections") {
-                                    echo "mega-menu-column--biggest";
-                                }
-                            ?> mega-menu-column">
-                                <ul>
-                                <?php
-                                    if ($item->title == "Shop vintage") {
-                                        echo "<li class=\"mega-menu-title\" id=\"shop-by-product\"><a href=\"#\">Shop by product</a></li>";
-                                    }
-                                ?>
-                                <?php // cycle through the menu items and get the subnav
-                                    foreach ($items as $subnav) {
-                                        $classList = implode(" ", $subnav->classes);
-                                        if ($subnav->menu_item_parent == $curNavItemID && in_array('subnav-column1', $subnav->classes)) {
-                                            echo "<li class=\"{$classList}\"><a href=\"{$subnav->url}\">{$subnav->title}</a></li>";
-                                            if ($item->title == "Shop collections") {
-                                                echo "<p>{$subnav->description}</p>"; 
-                                                ?>
-                                                </ul>
-                                                <img class="subnav-img" src="<?php bloginfo('template_directory');?>/images/wordart.png" alt="WordArt collection">
-                                            <?php                              
-                                            } 
-                                        }
-                                    } 
-                                ?>
-                                
-                            </div>
-                            <div class="<?php 
-                                if ($item->title == "Shop vintage") {
-                                    echo "mega-menu-column--small";
-                                }
-                                if ($item->title == "Shop collections") {
-                                    echo "mega-menu-column--biggest";
-                                }
-                            ?> mega-menu-column">
-                                <ul>
-                                <?php
-                                    if ($item->title == "Shop vintage") {
-                                        echo "<li class=\"mega-menu-title\" id=\"shop-by-brand\"><a href=\"#\">Shop by brand</a></li>";
-                                    }
-                                ?>
-                                <?php // cycle through the menu items and get the subnav
-                                    foreach ($items as $subnav) {
-                                        $classList = implode(" ", $subnav->classes);
-                                        if ($subnav->menu_item_parent == $curNavItemID && in_array('subnav-column2', $subnav->classes)) {
-                                            echo "<li class=\"{$classList}\"><a href=\"{$subnav->url}\">{$subnav->title}</a></li>";
-                                            if ($item->title == "Shop collections") {
-                                                echo "<p>{$subnav->description}</p>";
-                                                ?>
-                                                </ul>
-                                                <img class="subnav-img" src="<?php bloginfo('template_directory');?>/images/contant.png" alt="CONTANT collection">
-                                            <?php 
-                                            }
-                                        }
-                                    } 
-                                ?>
-                                
-                            </div>
-                            <?php
-                                if ($item->title == "Shop vintage") {
-                                    ?> <div class="mega-menu-column mega-menu-column--big">
-                                        <ul>
-                                            <li class="mega-menu-title mega-menu-title--new"><a href="#">What's new</a></li>
-                                            <div class="new-items-container">
-                                                <img src="<?php bloginfo('template_directory');?>/images/navimg1.jpg" alt="New products">
-                                                <img src="<?php bloginfo('template_directory');?>/images/navimg2.jpg" alt="New products">
-                                                <img src="<?php bloginfo('template_directory');?>/images/navimg3.jpg" alt="New products">
-                                            </div>
-                                        </ul>
-                                    </div>
-                                    <?php 
-                                }
-                            ?>
-                        </div>
-            <?php }
-                echo '</li>';
-                    }
-                }
-            } ?>
-            <div class="user-functions user-functions--mobile">
-                <div class="user-function--mobile">
-                    <div class="user-function-icon">
-                        <img src="<?php bloginfo('template_directory');?>/images/user-icon.svg" alt="User log in">
-                    </div>
-                    <a href="#">Log in</a>
-                </div>
-                <div class="user-function--mobile">
-                    <div class="user-function-icon">
-                        <img src="<?php bloginfo('template_directory');?>/images/heart-icon.svg" alt="Favourites">
-                    </div>    
-                    <a href="#">Faves</a>
-                </div>
-                <div class="user-function--mobile">
-                    <div class="user-function-icon">
-                        <img src="<?php bloginfo('template_directory');?>/images/bag-icon.svg" alt="Add to bag">
-                    </div>
-                    <a href="#">Bag</a>
-                </div>
+        <ul>
+            <div class="search-bar search-bar--mobile">
+                <input type="text" placeholder="Search product...">
+                <button type="submit"><img src="<?php bloginfo('template_directory');?>/images/search.svg" alt="Search product"></button>
             </div>
-    </ul>
-</div>
+
+            <?php $locations = get_nav_menu_locations();
+            // if there's a location for the primary menu
+            if (isset( $locations['top-menu'])) {
+                $menu = get_term( $locations['top-menu'], 'nav_menu');
+                // if there are items in the primary menu
+                if ($items = wp_get_nav_menu_items($menu->name)) {
+                    // loop through all menu items to display their content
+                    foreach ($items as $item) {
+                        // if the current item is not a top level item, skip it
+                        if ($item->menu_item_parent != 0) {
+                            continue;
+                        }
+                        // get the ID of the current nav item
+                        $curNavItemID = $item->ID;
+                        // get the custom classes for the item
+                        $classList = implode(" ", $item->classes);
+                        echo "<li class=\"{$classList}\">";
+                        echo "<a href=\"{$item->url}\">{$item->title}</a>";
+                        // build the mega-menu
+                        // if 'mega-menu'  exists within the class
+                        if (in_array('is-mega-menu', $item->classes)) { ?>
+                            <div class="mega-menu">
+                                <div class="<?php 
+                                    if ($item->title == "Shop vintage") {
+                                        echo "mega-menu-column--small";
+                                    }
+                                    if ($item->title == "Shop collections") {
+                                        echo "mega-menu-column--biggest";
+                                    }
+                                ?> mega-menu-column">
+                                    <ul>
+                                    <?php
+                                        if ($item->title == "Shop vintage") {
+                                            echo "<li class=\"mega-menu-title\" id=\"shop-by-product\"><a href=\"#\">Shop by product</a></li>";
+                                        }
+                                    ?>
+                                    <?php // cycle through the menu items and get the subnav
+                                        foreach ($items as $subnav) {
+                                            $classList = implode(" ", $subnav->classes);
+                                            if ($subnav->menu_item_parent == $curNavItemID && in_array('subnav-column1', $subnav->classes)) {
+                                                echo "<li class=\"{$classList}\"><a href=\"{$subnav->url}\">{$subnav->title}</a></li>";
+                                                if ($item->title == "Shop collections") {
+                                                    echo "<p>{$subnav->description}</p>"; 
+                                                    ?>
+                                                    </ul>
+                                                    <img class="subnav-img" src="<?php bloginfo('template_directory');?>/images/wordart.png" alt="WordArt collection">
+                                                <?php                              
+                                                } 
+                                            }
+                                        } 
+                                    ?>
+                                    
+                                </div>
+                                <div class="<?php 
+                                    if ($item->title == "Shop vintage") {
+                                        echo "mega-menu-column--small";
+                                    }
+                                    if ($item->title == "Shop collections") {
+                                        echo "mega-menu-column--biggest";
+                                    }
+                                ?> mega-menu-column">
+                                    <ul>
+                                    <?php
+                                        if ($item->title == "Shop vintage") {
+                                            echo "<li class=\"mega-menu-title\" id=\"shop-by-brand\"><a href=\"#\">Shop by brand</a></li>";
+                                        }
+                                    ?>
+                                    <?php // cycle through the menu items and get the subnav
+                                        foreach ($items as $subnav) {
+                                            $classList = implode(" ", $subnav->classes);
+                                            if ($subnav->menu_item_parent == $curNavItemID && in_array('subnav-column2', $subnav->classes)) {
+                                                echo "<li class=\"{$classList}\"><a href=\"{$subnav->url}\">{$subnav->title}</a></li>";
+                                                if ($item->title == "Shop collections") {
+                                                    echo "<p>{$subnav->description}</p>";
+                                                    ?>
+                                                    </ul>
+                                                    <img class="subnav-img" src="<?php bloginfo('template_directory');?>/images/contant.png" alt="CONTANT collection">
+                                                <?php 
+                                                }
+                                            }
+                                        } 
+                                    ?>
+                                </div>
+                                <?php
+                                    if ($item->title == "Shop vintage") {
+                                        ?> <div class="mega-menu-column mega-menu-column--big">
+                                            <ul>
+                                                <li class="mega-menu-title mega-menu-title--new"><a href="#">What's new</a></li>
+                                                <div class="new-items-container">
+                                                    <img src="<?php bloginfo('template_directory');?>/images/navimg1.jpg" alt="New products">
+                                                    <img src="<?php bloginfo('template_directory');?>/images/navimg2.jpg" alt="New products">
+                                                    <img src="<?php bloginfo('template_directory');?>/images/navimg3.jpg" alt="New products">
+                                                </div>
+                                            </ul>
+                                        </div>
+                                        <?php 
+                                    }
+                                ?>
+                            </div>
+                <?php }
+                    echo '</li>';
+                        }
+                    }
+                } ?>
+                <div class="user-functions user-functions--mobile">
+                    <div class="user-function--mobile">
+                        <div class="user-function-icon">
+                            <img src="<?php bloginfo('template_directory');?>/images/user-icon.svg" alt="User log in">
+                        </div>
+                        <a href="#">Log in</a>
+                    </div>
+                    <div class="user-function--mobile">
+                        <div class="user-function-icon">
+                            <img src="<?php bloginfo('template_directory');?>/images/heart-icon.svg" alt="Favourites">
+                        </div>    
+                        <a href="#">Faves</a>
+                    </div>
+                    <div class="user-function--mobile">
+                        <div class="user-function-icon">
+                            <img src="<?php bloginfo('template_directory');?>/images/bag-icon.svg" alt="Add to bag">
+                        </div>
+                        <a href="#">Bag</a>
+                    </div>
+                </div>
+            </ul>
+        </div>
 
         <div class="search-bar search-bar--desktop">
             <input type="text" placeholder="Search product...">
